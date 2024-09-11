125504590994e01e739aa9a2102a5c209bbcf007
RC_Student_lab <RC_Student_lab@10.0.0.70>
2024-09-11 13:07:19 +0000

    Added regex for Alphabets and Numbers Code was more shorter and simple

--- src/main/java/com/mycompany/githubtest/GithubTest.java
+++ src/main/java/com/mycompany/githubtest/GithubTest.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.githubtest;
 
+import java.util.Scanner;
+
 /**
  *
  * @author RC_Student_lab
@@ -11,7 +13,42 @@ package com.mycompany.githubtest;
 public class GithubTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int a;
+           Scanner sc= new Scanner(System.in);
+       System.out.println("Please enter username :"); 
+          ContainsandLength();
+           System.out.println("Please enter password :"); 
+          ContainsandLengthofPassword();
+
+          
     }
+       public static void ContainsandLength(){
+        Scanner sc= new Scanner(System.in);
+        String username = sc.next();
+       
+if(username.contains("_") || username.length()>6){
+    System.out.println("Username successfully captured ");
+}else{
+     System.out.println("Username is not correctly formatted");
+}   
+} 
+    public static void ContainsandLengthofPassword(){
+        Scanner sc= new Scanner(System.in);
+        String password = sc.next();
+        
+        if (password.length()>8 || password.matches(".*[A-Z].*")&&
+            password.length()>8 && password.matches(".*[0-9].*")&&
+            password.contains("@"  )||
+         password.contains("#")|| password.contains("$")|| password.contains("!")
+        || password.contains("%")|| password.contains("&")|| password.contains("*")|| password.contains("+")
+        || password.contains("~")
+        || password.contains("_")){
+   System.out.println("Password Successfully Captured"); 
+    }
+    else {
+    System.out.println("Password is not correctly formatted");
 }
+        }
+ }
+ 
+
+
