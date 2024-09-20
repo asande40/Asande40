3b38f5fc79bfbfdb26d322e08be9ce91d4e7f055
RC_Student_lab <RC_Student_lab@10.0.0.70>
2024-09-20 09:15:06 +0000

    Called my methods from Login3 into the main Edited some of the mothods Added correct messages

--- src/main/java/com/mycompany/githubtest/GithubTest.java
+++ src/main/java/com/mycompany/githubtest/GithubTest.java
@@ -3,52 +3,29 @@
  */
 
 package com.mycompany.githubtest;
-
 import java.util.Scanner;
+
 
 /**
  *
  * @author RC_Student_lab
  */
 public class GithubTest {
+    
 
     public static void main(String[] args) {
-           Scanner sc= new Scanner(System.in);
-       System.out.println("Please enter username :"); 
-          ContainsandLength();
-           System.out.println("Please enter password :"); 
-          ContainsandLengthofPassword();
-
-          
-    }
-       public static void ContainsandLength(){
         Scanner sc= new Scanner(System.in);
-        String username = sc.next();
+        Login3 login = new Login3();
+    
+          System.out.println("Please create username :");
+         login.registerUser();
+         
+           System.out.println("Please enter your login details.");
+          System.out.println("Username :");
+         
+          login.returnLoginStatus();
        
-if(username.contains("_") || username.length()>6){
-    System.out.println("Username successfully captured ");
-}else{
-     System.out.println("Username is not correctly formatted");
-}   
-} 
-    public static void ContainsandLengthofPassword(){
-        Scanner sc= new Scanner(System.in);
-        String password = sc.next();
         
-        if (password.length()>8 || password.matches(".*[A-Z].*")&&
-            password.length()>8 && password.matches(".*[0-9].*")&&
-            password.contains("@"  )||
-         password.contains("#")|| password.contains("$")|| password.contains("!")
-        || password.contains("%")|| password.contains("&")|| password.contains("*")|| password.contains("+")
-        || password.contains("~")
-        || password.contains("_")){
-   System.out.println("Password Successfully Captured"); 
-    }
-    else {
-    System.out.println("Password is not correctly formatted");
 }
-        }
- }
- 
-
-
+  
+}
