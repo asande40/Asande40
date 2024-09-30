a1ab010d40e3a20e8e125b169f65b4103493a046
asande40 <ST10451906@rcconnect.edu.za>
2024-09-30 13:58:10 +0200

    Update GithubTest.java
    NEW code

--- src/main/java/com/mycompany/githubtest/GithubTest.java
+++ src/main/java/com/mycompany/githubtest/GithubTest.java
@@ -14,21 +14,37 @@ public class GithubTest {
     
 
     public static void main(String[] args) {
+        
+        //Declaration of scanner and class
         Scanner sc= new Scanner(System.in);
         Login3 login = new Login3();
         
-         System.out.println("Please enter your name :");
-        login.FirstandLastName();
-    
-          System.out.println("Please create username :");
-         login.registerUser();
-         
-           System.out.println("Please enter your login details.");
-          System.out.println("Username :");
-         
-          login.returnLoginStatus();
-       
+        //Prompt user for name
+         System.out.println("Please enter your name below :");
+        String name=sc.next();
         
+        //Prompt user for lastname
+         System.out.println("Please enter your lastname below :");
+        String lastname=sc.next();
+        
+        //Prompt user to create username 
+       System.out.println("Please create your username below :");
+       String username=sc.next();
+        login.CheckUserName(username);
+        
+        //Prompt user to create password
+        System.out.println("Please create your  password below :");
+        String password=sc.next();
+        login.CheckPasswordComplexity(password);
+        login.registerUser(username, password);
+       
+       System.out.println("Please Login below");
+       System.out.println("Username :");
+       String LoginUsername=sc.next();
+       System.out.println("Password :");
+       String LoginUserPassword=sc.next();
+       
+       login.returnLoginStatus(LoginUsername, LoginUserPassword, password, username);
 }
   
 }
